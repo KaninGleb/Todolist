36fa4414d06e42a2e832d23f62b0071f5b8e5ae7
Kanin Gleb <gleb.kanin@gmail.com>
2025-05-31 18:19:23 +0300

    App - initializeAppTC dispatching added and CircularProgress added

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -1,20 +1,39 @@
-import "./App.css"
-import { selectThemeMode } from "@/app/app-slice"
-import { ErrorSnackbar, Header } from "@/common/components"
-import { useAppSelector } from "@/common/hooks"
-import { Routing } from "@/common/routing"
-import { getTheme } from "@/common/theme"
-import CssBaseline from "@mui/material/CssBaseline"
-import { ThemeProvider } from "@mui/material/styles"
+import './App.module.css'
+import { selectThemeMode } from '@/app/app-slice'
+import { ErrorSnackbar, Header } from '@/common/components'
+import { useAppDispatch, useAppSelector } from '@/common/hooks'
+import { Routing } from '@/common/routing'
+import { getTheme } from '@/common/theme'
+import CssBaseline from '@mui/material/CssBaseline'
+import { ThemeProvider } from '@mui/material/styles'
+import { useEffect, useState } from 'react'
+import { initializeAppTC } from '@/features/auth/model/auth-slice.ts'
+import s from './App.module.css'
+import { CircularProgress } from '@mui/material'
 
 export const App = () => {
+  const [isInitialized, setIsInitialized] = useState<boolean>(false)
   const themeMode = useAppSelector(selectThemeMode)
 
   const theme = getTheme(themeMode)
 
+  const dispatch = useAppDispatch()
+
+  useEffect(() => {
+    dispatch(initializeAppTC()).finally(() => setIsInitialized(true))
+  }, [])
+
+  if (!isInitialized) {
+    return (
+      <div className={s.circularProgressContainer}>
+        <CircularProgress size={150} thickness={3} />
+      </div>
+    )
+  }
+
   return (
     <ThemeProvider theme={theme}>
-      <div className={"app"}>
+      <div className={s.app}>
         <CssBaseline />
         <Header />
         <Routing />
