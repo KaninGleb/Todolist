53d10a63d15d06e9af8383e4c1db49829ffc6ca9
Kanin Gleb <gleb.kanin@gmail.com>
2025-05-31 18:16:34 +0300

    Login - useForm implemented

--- src/features/auth/ui/Login/Login.tsx
+++ src/features/auth/ui/Login/Login.tsx
@@ -1,23 +1,26 @@
-import { selectThemeMode } from "@/app/app-slice"
-import { useAppSelector } from "@/common/hooks"
-import { getTheme } from "@/common/theme"
-import { type Inputs, loginSchema } from "@/features/auth/lib/schemas"
-import { zodResolver } from "@hookform/resolvers/zod"
-import Button from "@mui/material/Button"
-import Checkbox from "@mui/material/Checkbox"
-import FormControl from "@mui/material/FormControl"
-import FormControlLabel from "@mui/material/FormControlLabel"
-import FormGroup from "@mui/material/FormGroup"
-import FormLabel from "@mui/material/FormLabel"
-import Grid from "@mui/material/Grid2"
-import TextField from "@mui/material/TextField"
-import { Controller, type SubmitHandler, useForm } from "react-hook-form"
-import styles from "./Login.module.css"
+import { selectThemeMode } from '@/app/app-slice'
+import { useAppDispatch, useAppSelector } from '@/common/hooks'
+import { getTheme } from '@/common/theme'
+import { type LoginInputs, loginSchema } from '@/features/auth/lib/schemas'
+import { zodResolver } from '@hookform/resolvers/zod'
+import Button from '@mui/material/Button'
+import Checkbox from '@mui/material/Checkbox'
+import FormControl from '@mui/material/FormControl'
+import FormControlLabel from '@mui/material/FormControlLabel'
+import FormGroup from '@mui/material/FormGroup'
+import FormLabel from '@mui/material/FormLabel'
+import Grid from '@mui/material/Grid2'
+import TextField from '@mui/material/TextField'
+import { Controller, type SubmitHandler, useForm } from 'react-hook-form'
+import styles from './Login.module.css'
+import { loginTC } from '@/features/auth/model/auth-slice.ts'
 
 export const Login = () => {
   const themeMode = useAppSelector(selectThemeMode)
 
   const theme = getTheme(themeMode)
+
+  const dispatch = useAppDispatch()
 
   const {
     register,
@@ -25,25 +28,25 @@ export const Login = () => {
     reset,
     control,
     formState: { errors },
-  } = useForm<Inputs>({
+  } = useForm<LoginInputs>({
     resolver: zodResolver(loginSchema),
-    defaultValues: { email: "", password: "", rememberMe: false },
+    defaultValues: { email: '', password: '', rememberMe: false },
   })
 
-  const onSubmit: SubmitHandler<Inputs> = (data) => {
-    console.log(data)
+  const onSubmit: SubmitHandler<LoginInputs> = (data) => {
+    dispatch(loginTC(data))
     reset()
   }
 
   return (
-    <Grid container justifyContent={"center"}>
+    <Grid container justifyContent={'center'}>
       <form onSubmit={handleSubmit(onSubmit)}>
         <FormControl>
           <FormLabel>
             <p>
               To login get registered
               <a
-                style={{ color: theme.palette.primary.main, marginLeft: "5px" }}
+                style={{ color: theme.palette.primary.main, marginLeft: '5px' }}
                 href="https://social-network.samuraijs.com"
                 target="_blank"
                 rel="noreferrer"
@@ -60,21 +63,21 @@ export const Login = () => {
             </p>
           </FormLabel>
           <FormGroup>
-            <TextField label="Email" margin="normal" error={!!errors.email} {...register("email")} />
+            <TextField label="Email" margin="normal" error={!!errors.email} {...register('email')} />
             {errors.email && <span className={styles.errorMessage}>{errors.email.message}</span>}
             <TextField
               type="password"
               label="Password"
               margin="normal"
               error={!!errors.email}
-              {...register("password")}
+              {...register('password')}
             />
             {errors.password && <span className={styles.errorMessage}>{errors.password.message}</span>}
             <FormControlLabel
-              label={"Remember me"}
+              label={'Remember me'}
               control={
                 <Controller
-                  name={"rememberMe"}
+                  name={'rememberMe'}
                   control={control}
                   render={({ field: { value, ...field } }) => <Checkbox {...field} checked={value} />}
                 />
